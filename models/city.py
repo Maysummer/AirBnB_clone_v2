e01e64617036e1e9c54d1ba95b0d48ce9da54536
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-14 18:48:50 +0000

    update place, city and user classes

--- models/city.py
+++ models/city.py
@@ -13,3 +13,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'),
                       nullable=False)
     state = relationship('State', back_populates='cities')
+    places = relationship('Place', back_populates='cities',
+                          cascade='all, delete, delete-orphan')
