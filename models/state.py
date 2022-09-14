a94a634c8dc0cf9897ebce6434a0148cb73318c5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-14 13:55:26 +0000

    update models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state',
+                          cascade="all, delete, delete-orphan")
+
+    def get_cities(self):
+        """returns all City objects associated with self"""
+        from models import storage
+        from models.city import City
+        cities = storage.all(City)
+        lst = []
+        for obj in cities.values():
+            if obj.state_id == self.id:
+                lst.append(obj)
+        return lst
